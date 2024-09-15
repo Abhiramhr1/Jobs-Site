5e1d6231bbb0dd10a5bee09d80a7ac46052b3cae
Abhiramhr1 <abhiramhr1.smg@gmail.com>
2024-09-15 12:08:31 +0530

    finish

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 const  connectDB = require('./db/connect')
@@ -14,8 +22,20 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
 
+// extra packages
+app.set('trust proxy',1)
+app.use(rateLimiter({
+  windowMs:15*60*1000,
+  max:100
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.get('/',(req,res)=>{
+  res.send('job api');
+})
 // routes
 app.get('/', (req, res) => {
   res.send('jobs api');
